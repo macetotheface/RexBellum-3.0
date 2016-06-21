e2451f188e66d0cc765cb898f9a02385736d5eda
MakeAmerciaGreatAgain <noel_pearce@hotmail.ca>
2016-06-21 19:26:01 -0400

    fixed play

--- src/game/Play.java
+++ src/game/Play.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.lwjgl.*;
 import org.lwjgl.input.Mouse;
 import mechanics.*;
@@ -11,9 +14,12 @@ import org.newdawn.slick.tiled.*;
 public class Play extends BasicGameState{
 	private Animation archerani;
 	//private boolean mousePressed;
+	
 	private Input input;
-	private int xpos = 50;
-	private int ypos = 0;
+	private int archerx = 200;
+	private int archery = 200;
+	private int xpos = 200;
+	private int ypos = 200;
 	private int tileID;
 	//private Image holder;
 	private TiledMap map;
@@ -37,7 +43,7 @@ public class Play extends BasicGameState{
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)throws SlickException{
-		g.scale(1f, 1f);
+		//g.scale(1f, 1f);
 		//g.scale(Display.getWidth()/720, Display.getHeight()/600);
 		map.render(0,0,0,-3,720,600);
 		//holder.draw(100,100,32,32);
@@ -50,22 +56,33 @@ public class Play extends BasicGameState{
 		g.drawRect(560, 149, 158, 25);
 		g.drawRect(560, 174, 75, 75);
 		g.drawRect(560, 174, 158, 250);
-		archerani.draw(200,200);
+		archerani.draw(archerx,archery);
 	
 	}
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException{
+		
 		input = gc.getInput();
-		//mousePressed = false;
+		int Water = map.getLayerIndex("Water 4");
 		xpos = Mouse.getX();
 		ypos = Mouse.getY();
 		mouse = "Mouse position x:" + xpos + "y" + ypos; 
-		//if(input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
-	     //    mousePressed = true;
-	     // }
-		if(input.isMousePressed(0)) {
-			tileID = map.getTileId(xpos,ypos,0);
+		boolean isMouseClicked = gc.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON);
+		int tileLocationX = Math.round(xpos)/map.getTileWidth();
+		int tileLocationY = Math.round(ypos)/map.getTileHeight();
+		if(isMouseClicked == true && ypos<560 && ypos>0 && xpos>0 && xpos<560) {
+			tileID = map.getTileId(tileLocationX,tileLocationY,Water);
+			gc.sleep(16);
 			System.out.println(tileID);
 		}
+		if(isMouseClicked == true && xpos==(archerx%-16) && (600-ypos)==(archery%-16)){
+			System.out.println("hi");
+			//if(isMouseClicked == true){
+				//System.out.println("hello");
+				//archerx=xpos;
+				//archery=(600-ypos);
+			}
+		//}
+			
 	}
 	
 	public int getID() {
