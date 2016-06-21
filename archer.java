61dc2fd0b8bb2a0c31158fd1ba9bdaebe16c424a
MakeAmerciaGreatAgain <noel_pearce@hotmail.ca>
2016-06-21 19:08:40 -0400

    Add files via upload

--- archer.java
+++ archer.java
@@ -1,11 +1,17 @@
 package mechanics;
+
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
 //unfinished
 public class archer extends unit {
-public archer(int faction){
+//private SpriteSheet = new SpriteSheet("/res/archer.png", 16,16);	
+public archer() throws SlickException{
 	this.setBaseRange(2);
 	this.setBaseHealth(100);
 	this.setBaseAttack(5);
 	this.setBaseMoves(2);
 	this.setCurrentAttack(this.getBaseAttack());
+	this.setSheet(new SpriteSheet("/res/archer.png", 16,16));
 }
 }
