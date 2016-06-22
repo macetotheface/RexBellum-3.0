28bf2ba7a1f70d6efe884386b90111095f4b4013
macetotheface <brandon.f.macdonald.1998@gmail.com>
2016-06-21 22:01:50 -0400

    Hopefully this works
    trying to get play class to work

--- src/game/Play.java
+++ src/game/Play.java
@@ -1,150 +1,3 @@
-<<<<<<< HEAD
-package game;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.lwjgl.*;
-import org.lwjgl.input.Mouse;
-import mechanics.*;
-import org.lwjgl.opengl.Display;
-import org.newdawn.slick.*;
-import org.newdawn.slick.state.*;
-import org.newdawn.slick.tiled.*;
-
-
-
-public class Play extends BasicGameState{
-	private Animation archerani;
-	//private boolean mousePressed;
-	
-	private Input input;
-	private int archerx = 200;
-	private int archery = 200;
-	private int xpos = 200;
-	private int ypos = 200;
-	private int tileID;
-	//private Image holder;
-	private TiledMap map;
-	//private boolean quit = false;
-	//private int[] duration = {200,200};
-	//private float positionX = 0;
-	//private float positionY = 0;
-	private int playerFaction = 4;
-	private Image factionCrest = null;
-	private String factionKing = null;
-	public Play(int state) throws SlickException{
-		//
-	}
-	public String mouse = "no input yet";
-	public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{	
-		archer hi = new archer();
-		archerani = new Animation(hi.getSheet(), 250);
-		map = new TiledMap("res/map.tmx");
-		
-		
-		//holder = new Image("res/placeholder.png");
-		
-	}
-	
-	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)throws SlickException{
-		factionCrest = getImage(playerFaction);
-		factionKing = getKing(playerFaction);
-		//g.scale(1f, 1f);
-		//g.scale(Display.getWidth()/720, Display.getHeight()/600);
-		map.render(0,0,0,-3,720,600);
-		//holder.draw(100,100,32,32);
-		//g.draw()
-		g.drawString(mouse, 50, 50);
-		g.drawRect(560, 0, 158, 47);
-		g.drawRect(560, 48, 158, 551);
-		g.drawRect(0, 0, 559, 47);
-		g.drawRect(560, 48, 158, 100);
-		g.drawRect(560, 149, 158, 25);
-		g.drawRect(560, 174, 75, 75);
-		g.drawRect(560, 174, 158, 250);
-		g.drawString(factionKing,605 , 13);
-		factionCrest.draw(565,7, 35, 35);
-		
-		archerani.draw(archerx,archery);
-	
-	}
-	public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException{
-		
-		input = gc.getInput();
-		int Water = map.getLayerIndex("Water 4");
-		xpos = Mouse.getX();
-		ypos = Mouse.getY();
-		mouse = "Mouse position x:" + xpos + "y" + ypos; 
-		boolean isMouseClicked = gc.getInput().isMousePressed(Input.MOUSE_LEFT_BUTTON);
-		int tileLocationX = Math.round(xpos)/map.getTileWidth();
-		int tileLocationY = Math.round(ypos)/map.getTileHeight();
-		if(isMouseClicked == true && ypos<560 && ypos>0 && xpos>0 && xpos<560) {
-			tileID = map.getTileId(tileLocationX,tileLocationY,Water);
-			gc.sleep(16);
-			System.out.println(tileID);
-		}
-		if(isMouseClicked == true && xpos==(archerx%-16) && (600-ypos)==(archery%-16)){
-			System.out.println("hi");
-			//if(isMouseClicked == true){
-				//System.out.println("hello");
-				//archerx=xpos;
-				//archery=(600-ypos);
-			}
-		//}
-			
-	}
-	
-	public int getID() {
-		return 1;
-	}
-	public Image getImage(int x) throws SlickException{
-		Image y = null;
-		
-		switch(x){
-		case 1:
-			//Human
-y = new Image("/res/Human_Faction_Crest.png");
-			break;
-		case 2:
-			//Elf
-y = new Image("/res/Elf_Faction_Crest.png");
-			break;
-		case 3:
-			//Dwarf
-y = new Image("/res/Dwarf_Faction_Crest.png");
-			break;
-		case 4://Orc
-y = new Image("/res/Orc_Faction_Crest.png");
-			break;
-	}
-		return y;
-	}
-	
-	public String getKing(int x) throws SlickException{
-		String y = null;
-		
-		switch(x){
-		case 1:
-			//Human
-			y = "King";
-			break;
-		case 2:
-			//Elf
-			y ="Tarostar";
-			break;
-		case 3:
-			//Dwarf
-			y = "Ra' Dawi";
-			break;
-		case 4://Orc
-			y = "Kri-krisur";
-			break;
-	}
-		return y;
-	}
-}
-=======
 package game;
 
 import java.util.ArrayList; 
@@ -178,6 +31,9 @@ public class Play extends BasicGameState{
 	//private float positionX = 0;
 	//private float positionY = 0;
 	
+	private int playerFactionType = 4;
+	private Image factionCrest = null;
+	private String factionKing = null;
 	private faction playerFaction = new faction(true);
 	private faction aiFaction1 = new faction(false);
 	private faction aiFaction2 = new faction(false);
@@ -206,6 +62,8 @@ public class Play extends BasicGameState{
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)throws SlickException{
+		factionCrest = getImage(playerFactionType);
+		factionKing = getKing(playerFactionType);
 		//g.scale(1f, 1f);
 		//g.scale(Display.getWidth()/720, Display.getHeight()/600);
 		map.render(0,0,0,-3,720,600);
@@ -222,6 +80,7 @@ public class Play extends BasicGameState{
 		archerani.draw(archerx,archery);
 		horseani.draw(100,100);
 		knightani.draw(300,300);
+		
 	
 	}
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException{
@@ -247,7 +106,52 @@ public class Play extends BasicGameState{
 				//archery=(600-ypos);
 			}
 		//}
-			
+	}
+	
+	public Image getImage(int x) throws SlickException {
+		Image y = null;
+
+		switch (x) {
+		case 1:
+			// Human
+			y = new Image("/res/Human_Faction_Crest.png");
+			break;
+		case 2:
+			// Elf
+			y = new Image("/res/Elf_Faction_Crest.png");
+			break;
+		case 3:
+			// Dwarf
+			y = new Image("/res/Dwarf_Faction_Crest.png");
+			break;
+		case 4:// Orc
+			y = new Image("/res/Orc_Faction_Crest.png");
+			break;
+		}
+		return y;
+	}
+	
+	public String getKing(int x) throws SlickException{
+		String y = null;
+		
+		switch(x){
+		case 1:
+			//Human
+			y = "King";
+			break;
+		case 2:
+			//Elf
+			y ="Tarostar";
+			break;
+		case 3:
+			//Dwarf
+			y = "Ra' Dawi";
+			break;
+		case 4://Orc
+			y = "Kri-krisur";
+			break;
+	}
+		return y;
 	}
 	
 	//Created Methods
@@ -265,7 +169,5 @@ public class Play extends BasicGameState{
 	public int getID() {
 		return 1;
 	}
-	
-	
 }
->>>>>>> origin/master
+
